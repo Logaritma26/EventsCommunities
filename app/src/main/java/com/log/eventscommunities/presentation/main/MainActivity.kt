11ab3af69826f7f8f81de6ff452f4f2600ad3e70
Logaritma26 <erolyildiz18it@gmail.com>
2022-02-18 10:43:36 +0100

    analytics, crashlytics and performance monitoring added

--- app/src/main/java/com/log/eventscommunities/presentation/main/MainActivity.kt
+++ app/src/main/java/com/log/eventscommunities/presentation/main/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.log.eventscommunities.presentation.screens.NavGraphs
 import com.log.eventscommunities.ui.theme.EventsCommunitiesTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -15,8 +18,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        firebaseAnalytics = Firebase.analytics
         setContent {
             EventsCommunitiesTheme {
                 DestinationsNavHost(navGraph = NavGraphs.root)
